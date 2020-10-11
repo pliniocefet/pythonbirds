3502427bf8deab5685d164e1cd5efd806c61fa72
pliniocefet <plinio.cefet@gmail.com>
2020-10-11 12:22:16 -0300

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 class Pessoa:
     
-    def __init__(self, nome=None, idade=36):
+    def __init__(self, *filhos, nome=None, idade=36):
+        # argumentos com 1 asterisco '*' na frente significa que ele não é obrigatorio e também poderá ter mais que 1
         # self.nome é o atributo da classe por isso self.
         # ja nome é o parametro que será atribuido ao atributo da classe
        self.nome = nome
        self.idade = idade
+       self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == "__main__":
-    p = Pessoa('Pai')
-    print(p.cumprimentar())
-    print(id(p))
-    print(p.nome)
-    p.nome = 'Plinio'
-    print(p.nome)
-    print(p.idade)
+    plinio = Pessoa(nome='Plinio')
+    rafael = Pessoa(nome='Rafael')
+    adair = Pessoa(rafael, plinio, nome='Adair')
+    print(adair.cumprimentar())
+    print(id(adair))
+    print(adair.nome)
+    print(adair.idade)
+    print(adair.filhos)
+
+    for filho in adair.filhos:
+        print(f'O nome dos Filhos de {adair.nome} é {filho.nome}')
+    
