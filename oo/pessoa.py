6c8b9b940f488205b446238e9883ef8d873fffc0
pliniocefet <plinio.cefet@gmail.com>
2020-10-11 11:38:19 -0300

    Criados atributos de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,20 @@
+# -*- coding: utf-8 -*-
 class Pessoa:
     
+    def __init__(self, nome=None, idade=36):
+        # self.nome é o atributo da classe por isso self.
+        # ja nome é o parametro que será atribuido ao atributo da classe
+       self.nome = nome
+       self.idade = idade
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == "__main__":
-    p = Pessoa()
+    p = Pessoa('Pai')
     print(p.cumprimentar())
     print(id(p))
+    print(p.nome)
+    p.nome = 'Plinio'
+    print(p.nome)
+    print(p.idade)
